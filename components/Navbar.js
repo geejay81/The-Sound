bf64735ebffe063239a58fc7faa3ddab566f1c21
Greg Parr <greg.parr@blueyonder.co.uk>
2024-03-26 14:24:30 +0000

    updated navbar and blog and project landing pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,13 +5,19 @@ import { faMusic } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
     return (
-        <nav className="max-w-screen-xl flex flex-wrap justify-between mx-auto p-4">
-            <Link href="/">
-                <div className="flex justify-between">
-                    <FontAwesomeIcon icon={faMusic} className="font-semibold text-xl px-3" />
-                    <span className="font-semibold text-xl tracking-tight">The Sound</span>
+        <nav className="relative container mx-auto p-6">
+            <div class="flex items-center justify-between">
+                <Link href="/">
+                    <a>
+                        <FontAwesomeIcon icon={faMusic} className="font-semibold text-xl px-3" />
+                        <span className="font-semibold text-xl tracking-tight">The Sound</span>
+                    </a>
+                </Link>
+                <div class="space-x-6">
+                    <Link href="/projects">Projects</Link>
+                    <Link href="/posts">Blog</Link>
                 </div>
-            </Link>
+            </div>
         </nav>
     );
 }
