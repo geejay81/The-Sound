8f985c1c501039fa0721217f16ba249ceb1a2708
Greg Parr <greg.parr@blueyonder.co.uk>
2024-04-06 22:52:16 +0100

    added cookie banner and google analytics

--- the-sound/app/(web)/layout.tsx
+++ the-sound/app/(web)/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/page/Navbar";
+import GoogleAnalytics from "@/components/page/GoogleAnalytics";
+import CookieBanner from "@/components/page/CookieBanner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +22,12 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <head>
         <meta name="google-adsense-account" content="ca-pub-9824524266019448" />
+        <GoogleAnalytics GA_MEASUREMENT_ID="G-DHN3V87D1E" />
       </head>
       <body className={`${inter.className}`}>
         <Navbar />
         {children}
+        <CookieBanner/>
       </body>
     </html>
   );
