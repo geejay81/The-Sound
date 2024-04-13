cdc173cba7db5aea80166bc00d8f604549fec4a6
Greg Parr <greg.parr@blueyonder.co.uk>
2024-04-13 22:06:12 +0100

    updated navbar to include icons

--- the-sound/app/(web)/games/page.tsx
+++ the-sound/app/(web)/games/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/page/Footer"
 import Hero from "@/components/page/Hero"
 import { Metadata } from "next"
 import Link from "next/link"
+import { FaGamepad } from "react-icons/fa"
 
 export const metadata: Metadata = {
     title: 'Games',
@@ -17,6 +18,7 @@ export default function GamesPage() {
             <main className="container mx-auto max-w-l p-6 md:pt-16 md:pb-10 space-y-3
                 flex flex-col md:flex-row space-x-3">
                 <div className="prose md:prose-lg">
+                    <p>This section has a list of games that I have been working on. Some of them link out to external sites, and others are available to play on this site.</p>
                     <h2>List of games to play</h2>
                     <ul>
                         <li><Link href="https://popidle.the-sound.co.uk">PopIdle</Link></li>
--- the-sound/components/page/Navbar.tsx
+++ the-sound/components/page/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import logo from "../../ui/fonts/Logo";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { FaGamepad, FaLaptop, FaQuoteLeft } from 'react-icons/fa';
 
 export default function Navbar() {
 
@@ -9,17 +8,20 @@ export default function Navbar() {
         {
             "title": "Projects",
             "url": "/projects",
-            "showInNavbar": true
+            "showInNavbar": true,
+            "icon": <FaLaptop className="inline" />
         },
         {
             "title": "Blog",
             "url": "/posts",
-            "showInNavbar": true
+            "showInNavbar": true,
+            "icon": <FaQuoteLeft className="inline" />
         },
         {
             "title": "Games",
             "url": "/games",
-            "showInNavbar": false
+            "showInNavbar": true,
+            "icon": <FaGamepad className="inline" />
         }
     ]
 
@@ -27,16 +29,18 @@ export default function Navbar() {
         <header className="bg-slate-700 text-white">
             <nav className="container mx-auto max-w-l flex items-center justify-between p-6">
                 <div className="text-2xl md:text-3xl">
-                    <Link href="/" className={logo.className}>The Sound</Link>
+                    <Link href="/" className={logo.className}>
+                        The Sound
+                    </Link>
                 </div>
-                <div className="space-x-5">
+                <div className="space-x-8">
                     {navLinks &&
                         navLinks
                             .filter((link: any) => link.showInNavbar)
                             .map((link: any) => (
-                            <Link key={link.url} href={link.url} className="space-x-2">
-                                {/* <FontAwesomeIcon icon={faFolder} /> */}
-                                <span>{link.title}</span>
+                            <Link key={link.url} href={link.url} title={link.title} className="space-x-2">
+                                {link.icon && link.icon}
+                                <span className="sr-only md:not-sr-only">{link.title}</span>
                             </Link>
                         ))
                     }
